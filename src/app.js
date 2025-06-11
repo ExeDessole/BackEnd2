47c1e56519fe31a469c24443127f5cdaaecb9f03
ExeDessole <dessolem@yahoo.com>
2025-06-11 19:01:41 -0300

    Cookies and sessions

--- src/app.js
+++ src/app.js
@@ -1,14 +1,28 @@
 import express, {json, urlencoded} from "express"
+import cookieParser from "cookie-parser";
+import session from "express-session";
 import mongoose from "mongoose"
 import morgan from "morgan";
+import dotenv from 'dotenv';
 
+// Config variables de entorno
+dotenv.config();
+
+// Declaración de APP y PORT
 const APP = express();
 const PORT = process.env.PORT;
 
+// Middlewares para APP
 APP.use(json());
 APP.use(urlencoded({ extended: true }));
 APP.use(morgan("dev"));
 
+// Inicio de servidor Express
 APP.listen(PORT, ()=>{
     console.log(`Listening port: ${PORT}`)
 });
+
+// Rutas generales
+APP.get("/", (req,res) =>{
+    res.send("Pagina principal")
+})
