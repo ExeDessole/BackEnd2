bc8acfcf904d427bb4a8bf7d00e56f72adf52d2c
ExeDessole <dessolem@yahoo.com>
2025-06-17 17:32:00 -0300

    cookies

--- src/app.js
+++ src/app.js
@@ -3,26 +3,46 @@ import cookieParser from "cookie-parser";
 import session from "express-session";
 import mongoose from "mongoose"
 import morgan from "morgan";
-import dotenv from 'dotenv';
-
-// Config variables de entorno
-dotenv.config();
 
 // Declaración de APP y PORT
-const APP = express();
-const PORT = process.env.PORT;
+const app = express();
+const {PORT,SECRET} = process.env;
 
 // Middlewares para APP
-APP.use(json());
-APP.use(urlencoded({ extended: true }));
-APP.use(morgan("dev"));
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(cookieParser(SECRET));
 
 // Inicio de servidor Express
-APP.listen(PORT, ()=>{
+app.listen(PORT, ()=>{
     console.log(`Listening port: ${PORT}`)
 });
 
 // Rutas generales
-APP.get("/", (req,res) =>{
-    res.send("Pagina principal")
+app.get("/", (req,res) =>{
+    res.cookie("CookiePrueba", "valor de la cookie", {
+        maxAge: 10000,
+        sameSite: true, 
+        signed: true,
+        httpOnly: true
+    })
+    res.send("Pagina principal CON COOKIE")
+})
+
+app.get("/view-cookie", (req,res) =>{
+    let cookie_datos = req.cookies;
+    res.send(`Datos de la cookie: ${cookie_datos}`)
+})
+
+app.get("/register", (req,res) =>{
+    res.send("Pagina de registro")
+})
+
+app.get("/login", (req,res) =>{
+    res.send("Pagina de login")
+})
+
+app.get("/protegida", (req,res) =>{
+    res.send("Pagina protegida")
 })
