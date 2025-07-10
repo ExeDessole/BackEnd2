9d2a73f706ba9837f0c9210641217af59b4def3d
Exe Dessole <dessolem@yahoo.com>
2025-07-10 02:31:43 -0300

    jwt

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,1 @@
-import { genSaltSync } from "bcrypt";
-import bcrypt from "bcryptjs";
-
-export const createHash = password => bcrypt.hashSync((password, genSaltSync(10)));
-export const isValidPassword = (user, password) => bcrypt.compareSync(password, user.password);
-
+hnhn
