ffa2f650d657fcd60ac5e92586ad366b25b77566
ExeDessole <dessolem@yahoo.com>
2025-07-14 18:49:37 -0300

    avance Lunes

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,42 +1,11 @@
 import { Router } from "express";
-import userModel from "../DAOS/mongo/models/userModel.js";
 
 const usersRouter = Router();
 
-//CRUD USER
-//RETURN USER LIST
-usersRouter.get("/", async (req,res) =>{
-  try {
-    res.status(200).json(await userModel.find());
-  } catch (error) {
-    res.status(500).json({
-      error: "Error al obtener usuarios",
-      details: error.message,
-    });
-  }
-});
+//CRUD LIMITADO USER SOLO C
 //RETURN USER BY ID 
-usersRouter.get("/:id", async (req,res) =>{
-  try {
-    res.json(await userModel.findById(req.params.id))
-  } catch (error) {
-    res.status(500).json({
-      error: "Usuario no encontrado",
-      details: error.message
-    });
-  }
-});
-
+usersRouter.get("/:id", );
 //DELETE USER BY ID
-usersRouter.delete("/:id", async (req,res) =>{
-  try {
-    res.json(await userModel.findByIdAndDelete({_id: req.params.id}));
-  } catch (error) {
-    res.status(500).json({
-      error: "No se pudo eliminar el usuario",
-      details: error.message
-    });
-  }
-});
+usersRouter.delete("/:id", );
 
 export default usersRouter;
